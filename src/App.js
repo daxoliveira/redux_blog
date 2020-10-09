177af65dc484a15dfb8a1b27226bc4ca94119ec9
daxoliveira <dax-oliveira-br@gmail.com>
2020-10-08 22:51:57 -0300

    Include Redux logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
+import reactLogo from './reactLogo.svg';
+import reduxLogo from './reduxLogo.svg';
 import './App.css';
 
 import Posts from './components/Posts';
@@ -8,7 +9,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={reactLogo} className="App-react-logo" alt="react logo" />
+        <img src={reduxLogo} className="App-redux-logo" alt="redux logo" />
         <p>Welcome to React</p>
       </header>
       <Posts />
