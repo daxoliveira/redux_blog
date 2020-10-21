bd73b539dc1a4faf48882509a2bf7e6d5e2e05a1
daxoliveira <dax-oliveira-br@gmail.com>
2020-10-21 17:10:31 -0500

    Add thunk and root reducer to the store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,21 @@
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+// The second argument in the createStore function is the initial state
+const initialState = {};
+
+// The third argument in the createStore function is supposed to be an enhancer.
+// The redux thunk middleware is just that, an enhancer that will help with async actions.
+const middleware = [thunk];
 
 // The store holds the whole state tree of your application. 
 // The only way to change the state inside it is to dispatch an action on it.
 // To create the store object pass your root reducing function to createStore.
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(...middleware)
+);
+
+export default store;
