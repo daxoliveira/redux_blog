b3c00b55395234ab3c5a2f27bbdaf1add6be6186
daxoliveira <dax-oliveira-br@gmail.com>
2020-10-21 21:51:40 -0500

    Create the reducer and its actions

---
+++ src/actions/types.js
@@ -0,0 +1,4 @@
+// These are the two actions that will be sent to the postReducer
+
+export const FETCH_POSTS = 'FETCH_POSTS';
+export const NEW_POSTS = 'NEW_POSTS';
---
+++ src/reducers/postReducer.js
@@ -0,0 +1,14 @@
+// A reducer basically takes actions that will manipulate the state.
+import { FETCH_POSTS, NEW_POSTS } from '../actions/types';
+
+const initialState = {
+  items: [],
+  item: {}
+}
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
