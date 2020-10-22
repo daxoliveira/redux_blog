b447e3f5d6f23ae7e90d330f024e3a49085480f7
daxoliveira <dax-oliveira-br@gmail.com>
2020-10-21 22:37:29 -0500

    Implement fetchPosts action

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/postActions';
 
 class Posts extends Component {
   // The state and the fetch call to the fake API that used to be here were moved to the action creator function in postActions.js
+
+  componentWillMount() {
+    this.props.fetchPosts(); 
+  }
+
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
       </div>
-    ))
+    ));
     return (
       <div>
         <h1>Posts</h1>
@@ -18,4 +25,8 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+const mapStateToProps = state => ({
+  posts: state.posts.items
+})
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
